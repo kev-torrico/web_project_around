4719231c951fd9d90410bc5387462a7df4e0f9d7
KevinVillca <kevtorricov@gmail.com>
2025-12-06 01:24:48 -0400

    Feat: Create extended class for PopupWithForm and modify inherited methods

---
+++ scripts/PopupWithForm.js
@@ -0,0 +1,28 @@
+import { Popup } from "./Popup";
+
+export class PopupWithForm extends Popup {
+  constructor(popupSelector, handleProfileFormSubmit) {
+    super(popupSelector);
+    this._handleProfileFormSubmit = handleProfileFormSubmit;
+    this._form = this._popup.querySelector(".popup__form");
+    this._inputList = this._popup.querySelectorAll(".popup__input-form");
+  }
+  _getInputValues() {
+    const formValues = {};
+    this._inputList.forEach((input) => {
+      formValues[input.id] = input.value;
+    });
+    return entryFields;
+  }
+  setEventListeners(closeButtonSelector) {
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleProfileFormSubmit(this._getInputValues());
+    });
+    super.setEventListeners(closeButtonSelector);
+  }
+  close() {
+    super.close();
+    this._form.reset();
+  }
+}
