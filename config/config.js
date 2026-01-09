7e0a67a42ade84b5d1b68142056c302b0faacde4
KevinVillca <kevtorricov@gmail.com>
2026-01-09 00:48:49 -0400

    Feat: Create config variable to set baseUrl and headers

---
+++ config/config.js
@@ -0,0 +1,7 @@
+export const apiConfig = {
+  baseUrl: "https://around-api.es.tripleten-services.com/v1/",
+  headers: {
+    token: "8e66e974-63b5-4fbf-a2ed-12a950290f95",
+    contentType: "application/Json",
+  },
+};
