0ac01dc2210ab7749b13e189b4ef6d05889621f5
KevinVillca <kevtorricov@gmail.com>
2026-01-09 00:51:08 -0400

    refactor: Based POO separation of fetching user info and add update profile method

--- scripts/UserInfo.js
+++ scripts/UserInfo.js
@@ -1,10 +1,26 @@
 export class UserInfo {
-  constructor({ userName, userJob }) {
+  constructor({ userName, userJob, baseUrl, headers }) {
     this._userName = document.querySelector(userName);
     this._userJob = document.querySelector(userJob);
     this._userImage = document.querySelector(".profile__img");
+    this._baseUrl = baseUrl;
+    this._headers = headers;
   }
   getUserInfo() {
+    this._request(`${this._baseUrl}/users/me`, {
+      method: "GET",
+      headers: this._headers,
+    })
+      .then((result) => {
+        this.setUserInfo({
+          name: result.name,
+          job: result.about,
+          profileImage: result.avatar,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     return {
       name: this._userName.textContent,
       job: this._userJob.textContent,
@@ -15,5 +31,26 @@ export class UserInfo {
     this._userName.textContent = name;
     this._userJob.textContent = job;
     this._userImage.src = profileImage || "../images/profile-img.jpg";
+  }
+
+  updateProfile(data) {
+    return this._request(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify(data),
+    }).then((result) => {
+      this.setUserInfo({
+        name: result.name,
+        job: result.about,
+      });
+    });
+  }
+  _request(url, options) {
+    return fetch(url, options).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Algo ha fallado: ${res.status}`);
+    });
   }
 }
