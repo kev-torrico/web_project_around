daf09f84443aafbe5a0097f3627e1801bc2a718f
KevinVillca <kevtorricov@gmail.com>
2026-01-09 01:50:17 -0400

    refactor: Add fetching method for creating cards with post

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,9 +1,12 @@
 export class Card {
-  constructor(data, cardTemplate, handleCardClick) {
+  constructor(data, cardTemplate, handleCardClick, baseUrl, headers) {
+    this._data = data;
     this._text = data.name;
     this._image = data.link;
     this._cardTemplate = cardTemplate;
     this._handleCardClick = handleCardClick;
+    this._baseUrl = baseUrl;
+    this._headers = headers;
   }
   _getTemplate() {
     const cardElement = document
@@ -46,5 +49,21 @@ export class Card {
   _handleDeleteClick(evt) {
     const cardToDelete = evt.target.closest(".card");
     cardToDelete.remove();
+  }
+  createCard() {
+    this._request(`${this._baseUrl}/cards/`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify(this._data),
+    });
+    return console.log("card creada");
+  }
+  _request(url, options) {
+    return fetch(url, options).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Algo ha fallado: ${res.status}`);
+    });
   }
 }
