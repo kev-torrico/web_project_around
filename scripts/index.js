99ff3cc7d28260bfa48c2f68cbb27764a808dcf6
KevinVillca <kevtorricov@gmail.com>
2026-01-09 00:54:50 -0400

    refactor: Method's initiation of getting cards section and users information

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,8 @@ import { UserInfo } from "./UserInfo.js";
 import { PopupWithForm } from "./PopupWithForm.js";
 import { PopupWithImage } from "./PopupWithImage.js";
 import { FormValidator } from "./FormValidator.js";
-import { formValidators, validationConfig } from "./utils.js";
+import { formValidators, validationConfig } from "./Utils.js";
+import { apiConfig } from "../config/config.js";
 
 // Validacion de formularios
 const formList = Array.from(
@@ -20,36 +21,18 @@ formList.forEach((formElement) => {
 
 // ******   Usuarios   *******
 // Instanciamos un usuario
-const userInfo = new UserInfo({
+export const userInfo = new UserInfo({
   userName: ".profile__name",
   userJob: ".profile__job",
+  baseUrl: apiConfig.baseUrl,
+  headers: {
+    "Content-Type": apiConfig.headers.contentType,
+    Authorization: apiConfig.headers.token,
+  },
 });
 
 //Obtener informacion del usuario desde el servidor
-
-fetch("https://around-api.es.tripleten-services.com/v1/users/me", {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-    authorization: "8e66e974-63b5-4fbf-a2ed-12a950290f95",
-  },
-})
-  .then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Algo ha fallado: ${res.status}`);
-  })
-  .then((result) => {
-    userInfo.setUserInfo({
-      name: result.name,
-      job: result.about,
-      profileImage: result.avatar,
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+userInfo.getUserInfo();
 
 // Instanciamos un Popup con la imagen y añadimos escuchadores de eventos
 const popupWithImage = new PopupWithImage(".popup_view-img");
@@ -63,7 +46,7 @@ const handleCardClick = (cardName, cardImage) => {
 // **** Secion de las tarjetas ****
 
 // Contenedor de las tarjetas
-const cardsSection = new Section(
+export const cardsSection = new Section(
   {
     items: [],
     renderer: (item) => {
@@ -71,53 +54,17 @@ const cardsSection = new Section(
       const cardElement = card.generateCard();
       cardsSection.addItem(cardElement);
     },
+    baseUrl: apiConfig.baseUrl,
+    headers: {
+      "Content-Type": apiConfig.headers.contentType,
+      Authorization: apiConfig.headers.token,
+    },
   },
   ".cards-container"
 );
 
 // Obtencion de informacion acerca de las tarjetas.
-fetch("https://around-api.es.tripleten-services.com/v1/cards/", {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/Json",
-    Authorization: "8e66e974-63b5-4fbf-a2ed-12a950290f95",
-  },
-})
-  .then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Algo ha fallado ${res.status}`);
-  })
-  .then((cards) => {
-    cardsSection.setItems(cards);
-    cardsSection.renderItems();
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-// Popup de perfil
-
-const popupEditProfile = new PopupWithForm(".popup_profile", (formData) => {
-  userInfo.setUserInfo({
-    name: formData["name-input"],
-    job: formData["job-input"],
-  });
-});
-popupEditProfile.setEventListeners(".popup__close");
-
-// Abrir el boton de edit del popup de perfil
-document
-  .querySelector(".profile__button-edit")
-  .addEventListener("click", () => {
-    const currentInfo = userInfo.getUserInfo();
-    popupEditProfile.setInputValues({
-      "name-input": currentInfo.name,
-      "job-input": currentInfo.job,
-    });
-    popupEditProfile.open();
-  });
+cardsSection.getItems();
 
 // Popup para añadir el card
 
@@ -140,3 +87,25 @@ document.querySelector(".profile__button-add").addEventListener("click", () => {
   popupAddCard.close();
   popupAddCard.open();
 });
+
+// **** Edicion de usuarios y popup del perfil****
+// Popup de perfil
+const popupEditProfile = new PopupWithForm(".popup_profile", (formData) => {
+  userInfo.updateProfile({
+    name: formData["name-input"],
+    about: formData["job-input"],
+  });
+});
+popupEditProfile.setEventListeners(".popup__close");
+
+// Abrir el boton de edit del popup de perfil
+document
+  .querySelector(".profile__button-edit")
+  .addEventListener("click", () => {
+    const currentInfo = userInfo.getUserInfo();
+    popupEditProfile.setInputValues({
+      "name-input": currentInfo.name,
+      "job-input": currentInfo.job,
+    });
+    popupEditProfile.open();
+  });
