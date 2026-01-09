4d2a8e2fb6f42ef4b772f9993ee5c16529f0b490
KevinVillca <kevtorricov@gmail.com>
2026-01-09 00:52:29 -0400

    refactor: POO based getter function with fetching

--- scripts/Section.js
+++ scripts/Section.js
@@ -1,8 +1,10 @@
 export class Section {
-  constructor({ items, renderer }, containerSelector) {
+  constructor({ items, renderer, baseUrl, headers }, containerSelector) {
     this._initialArray = items;
     this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
+    this._baseUrl = baseUrl;
+    this._headers = headers;
   }
   renderItems() {
     this._initialArray.forEach((item) => {
@@ -14,5 +16,27 @@ export class Section {
   }
   setItems(items) {
     this._initialArray = items;
+  }
+  getItems() {
+    this._request(`${this._baseUrl}/cards/`, {
+      method: "GET",
+      headers: this._headers,
+    });
+  }
+  _request(url, options) {
+    return fetch(url, options)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Algo ha fallado ${res.status}`);
+      })
+      .then((cards) => {
+        this.setItems(cards);
+        this.renderItems(cards);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
