9d76b26fc7c4dd654b84eb6f35c9ededcb4e737d
KevinVillca <kevtorricov@gmail.com>
2026-02-03 20:58:41 -0400

    fix: Add ID property for cards

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,10 +1,13 @@
 export class Card {
-  constructor(data, cardTemplate, handleCardClick) {
+  constructor(data, cardTemplate, handleCardClick, handleDeleteClick, cardApi) {
     this._data = data;
     this._text = data.name;
     this._image = data.link;
+    this._id = data._id;
     this._cardTemplate = cardTemplate;
     this._handleCardClick = handleCardClick;
+    this._handleDeleteClick = handleDeleteClick;
+    this._cardApi = cardApi;
   }
   _getTemplate() {
     const cardElement = document
@@ -23,12 +26,20 @@ export class Card {
     this._element.querySelector(".card__img").src = this._image;
     this._element.querySelector(".card__img").alt = this._text;
     this._element.querySelector(".card__title").textContent = this._text;
+
+    const likeIcon = this._element.querySelector(".card__like span");
+
+    if (this._data.isLiked === true) {
+      likeIcon.classList.add("material-symbols-outlined-fill");
+    } else {
+      likeIcon.classList.remove("material-symbols-outlined-fill");
+    }
   }
   _setEventListeners() {
     this._element
       .querySelector(".card__delete")
-      .addEventListener("click", (evt) => {
-        this._handleDeleteClick(evt);
+      .addEventListener("click", () => {
+        this._handleDeleteClick(this);
       });
     this._element
       .querySelector(".card__like")
@@ -41,11 +52,33 @@ export class Card {
     });
   }
   _handleLikeClick(evt) {
-    const eventTarget = evt.target.closest("span");
-    eventTarget.classList.toggle("material-symbols-outlined-fill");
+    const likeIcon = this._element.querySelector(".card__like span");
+    const isCurrentlyLiked = this._data.isLiked;
+
+    if (isCurrentlyLiked) {
+      this._cardApi
+        .unLikeCard(this.getId())
+        .then((updatedCard) => {
+          this._data.isLiked = updatedCard.isLiked;
+          likeIcon.classList.remove("material-symbols-outlined-fill");
+        })
+        .catch((err) => console.error(err));
+    } else {
+      this._cardApi
+        .likeCard(this.getId())
+        .then((updatedCard) => {
+          this._data.isLiked = updatedCard.isLiked;
+          likeIcon.classList.add("material-symbols-outlined-fill");
+        })
+        .catch((err) => console.error(err));
+    }
   }
-  _handleDeleteClick(evt) {
-    const cardToDelete = evt.target.closest(".card");
-    cardToDelete.remove();
+
+  getId() {
+    return this._id;
+  }
+  removeCard() {
+    this._element.remove();
+    this._element = null;
   }
 }
